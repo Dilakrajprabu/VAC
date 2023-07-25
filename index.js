ee37d4e789ffb4fb433f81675c8a2a5335132f47
dilakrajprabu <dilakprabujp03@gmail.com>
2023-07-25 13:06:11 +0530

    message

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Dilak");
+
+//this one
+/*
+console.log("hello");
+console.log("hello");
+*/
+
+
+
+let name = "Dilak";
+let age = 23;
+let completedCollege = false;
+let tech = ["app" , "website" , "backend"];
+
